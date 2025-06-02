1722952ab236a9cbe2ec7c29cf73acb18ca52e80
Fábio Henrique <fabiohenriquesf8@gmail.com>
2025-06-02 20:08:03 -0300

    feat: added control node

---
+++ apps/control-node/provision.sh
@@ -0,0 +1,22 @@
+﻿#!/bin/sh
+
+echo "Updating apt"
+sudo apt update
+
+echo "Installing software-properties-common"
+sudo apt install software-properties-common
+
+echo "Adding ansible PPA"
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+
+echo "Installing ansible"
+sudo apt install -y ansible
+
+echo "Updating /etc/hosts"
+cat <<EOT > /etc/hosts
+192.168.56.2 control-node
+192.168.56.3 app01
+192.168.56.4 db01
+EOT
+
+echo "Provisioning finished!"
--- provision.sh
+++
@@ -1,8 +0,0 @@
-﻿#!/usr/bin/env bash
-echo "Instalando Apache e executando setup..."
-
-sudo apt update
-
-sudo apt install -y apache2
-
-cp -r /vagrant/html/* /var/www/html
